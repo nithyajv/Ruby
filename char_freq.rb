7b40bb9e02af4b231727bef72561288f709fabd8
nithyajv <64721654+nithyajv@users.noreply.github.com>
2022-08-09 10:09:06 +0530

    Update char_freq.rb

--- char_freq.rb
+++ char_freq.rb
@@ -6,3 +6,15 @@ puts "Enter the character"
 char=gets.chomp
 puts string.count(char)
 
+#Method 2
+def iter(string,char)
+  c=0
+  string.each_char do |x|
+    if char==x
+      c=c+1
+    end 
+  end
+  
+  return c
+end
+puts iter(string,char)
